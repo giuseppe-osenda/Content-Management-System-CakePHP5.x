aa4f5d01a29355bf649b767970503cfe99813ed7
Giuseppe Osenda <49283104+giuseppe-osenda@users.noreply.github.com>
2024-03-23 17:42:07 +0100

    Initial commit

---
+++ src/Controller/Admin/PagesController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Controller\Admin;
+
+    use App\Controller\Admin\AppController;
+
+    class PagesController extends AppController
+    {
+        public function index()
+        {
+    
+        }
+    }
---
+++ templates/Admin/Pages/index.php
@@ -0,0 +1,1 @@
+<?php $this->setLayout('CakeLte.default') ?>
